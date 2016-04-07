9a26896620d48645ad192c7229cf77e2ca874873
Jesse <j.h.de@Hotmail.com>
2016-04-07 15:20:23 +0200

    Workspace
    Ignored .metadata, .recommenders, .settings, .classpath and lib
    Set classpath yourself and add jade.jar and commons codec in \lib folder

---
+++ AuctioneerWorkspace/Auctioneer/src/Auctioneer.java
@@ -0,0 +1,36 @@
+import jade.core.*;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.*;
+import jade.domain.FIPAException;
+
+public class Auctioneer extends Agent {
+	protected void register()
+	{
+	 	DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName( getAID() ); 
+        ServiceDescription sd  = new ServiceDescription();
+        sd.setType( "seller" );
+        sd.setName( getLocalName() );
+        dfd.addServices(sd);
+        
+        try {  
+            DFService.register(this, dfd );  
+        }
+        catch (FIPAException fe) { fe.printStackTrace(); }
+	}
+	protected DFAgentDescription[] getBuyers(int n){
+		DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd  = new ServiceDescription();
+        sd.setType( "buyer" );
+        dfd.addServices(sd);
+        
+        DFAgentDescription[] result;
+		try {
+			result = DFService.search(this, dfd);
+		} catch (FIPAException e) {
+			result = null;
+			e.printStackTrace();
+		}
+        return result;
+	}
+}
